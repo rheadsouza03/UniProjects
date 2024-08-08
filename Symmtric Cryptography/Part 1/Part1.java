66c40f97403fcece4f593ffe188eb8df100fb79e
Rhea D'Souza <97720582+rheadsouza03@users.noreply.github.com>
2024-08-08 12:58:18 +1200

    Fixed most of the issues. The program works as it is supposed to, for the most part. The iv and key increment, however, needs to be resolved. Only works when multiple commands are inputted at a time.

--- Symmtric Cryptography/Part 1/Part1.java
+++ Symmtric Cryptography/Part 1/Part1.java
@@ -17,8 +17,11 @@ import javax.crypto.Cipher;
 import javax.crypto.CipherInputStream;
 import javax.crypto.CipherOutputStream;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+
+import static java.lang.System.exit;
 
 /**
  * Commandline based encryption and decryption program
@@ -29,10 +32,16 @@ public class Part1 {
 
     private static final String ALGORITHM = "AES";
     private static final String CIPHER = "AES/CBC/PKCS5PADDING";
+    private static final int GCM_TAG_LENGTH = 128;
+    private static int ivIncrement = 0;
+    private static int keyIncrement = 0;
 
-    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IOException {
+    public static void main(String[] args){
         // Handling commandline arguments
-        if(args.length < 2) {LOG.log(Level.SEVERE, "Input file is required. Cannot perform operation.");}
+        if(args.length < 2) {
+            LOG.log(Level.SEVERE, "Input file is required. Cannot perform operation.");
+            exit(1);
+        }
         String encOrDec = args[0].toLowerCase();
         Map<String, String> arguments = parseArgs(args);
 
@@ -46,16 +55,32 @@ public class Part1 {
         // Check to ensure decryption operation has a key and iv specified
         if (encOrDec.equals("dec") && (skeySpec == null || iv == null)) {
             LOG.log(Level.SEVERE, "Decryption requirement not met. No initialisation-vector and/or key specified.");
-            return;
+            exit(1);
         }
 
         // Gets mode of Cipher
-        Cipher cipher = Cipher.getInstance(arguments.getOrDefault("mode", CIPHER));
+        String cipherMode = arguments.getOrDefault("mode", CIPHER);
+        if(cipherMode.equals("GCM") || cipherMode.equals("CFB") || cipherMode.equals("OFB")) {
+            cipherMode = "AES/"+cipherMode+"/NoPadding";
+        }
+        Cipher cipher = null;
+        try {
+            cipher = Cipher.getInstance(cipherMode);
+        } catch (NoSuchAlgorithmException e) {
+            LOG.log(Level.SEVERE, "Algorithm not supported: " + cipherMode, e);
+            exit(1);
+        } catch (NoSuchPaddingException e) {
+            LOG.log(Level.SEVERE, "No such padding: " + cipherMode, e);
+            exit(1);
+        }
 
         //Perform Encryption or Decryption
-        if(encOrDec.equals("enc")) {performEncryption(cipher, iv, skeySpec, arguments);}
-        else if(encOrDec.equals("dec")) {performDecryption(cipher, iv, skeySpec, arguments);}
-        else{ LOG.log(Level.SEVERE, "Unrecognized mode. Use 'ENC' for encryption or 'DEC' for decryption");}
+        if(encOrDec.equals("enc")) {performEncryption(cipher, iv, skeySpec, arguments, cipherMode);}
+        else if(encOrDec.equals("dec")) {performDecryption(cipher, iv, skeySpec, arguments, cipherMode);}
+        else{
+            LOG.log(Level.SEVERE, "Unrecognized mode. Use 'ENC' for encryption or 'DEC' for decryption");
+            exit(1);
+        }
     }
 
     /**
@@ -67,11 +92,23 @@ public class Part1 {
      * @throws InvalidAlgorithmParameterException
      * @throws InvalidKeyException
      */
-    private static void performDecryption(Cipher cipher, IvParameterSpec iv, SecretKeySpec skeySpec, Map<String, String> arguments) throws InvalidAlgorithmParameterException, InvalidKeyException {
-        cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
-
+    private static void performDecryption(Cipher cipher, IvParameterSpec iv, SecretKeySpec skeySpec, Map<String, String> arguments, String cipherMode){
+        try {
+            if (cipherMode.contains("GCM")) {
+                cipher.init(Cipher.DECRYPT_MODE, skeySpec, new GCMParameterSpec(GCM_TAG_LENGTH, iv.getIV()));
+            } else {
+                cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+            }
+        }catch (InvalidKeyException e) {
+            LOG.log(Level.SEVERE, "Invalid key. Cannot be used for decryption of this file.");
+            exit(1);
+        } catch (InvalidAlgorithmParameterException e) {
+            LOG.log(Level.SEVERE, "Invalid algorithm. Cannot be used for decryption of this file.");
+            exit(1);
+        }
         Path inputPath = Paths.get(arguments.getOrDefault("input-file", null));
-        Path outputPath = Paths.get(arguments.getOrDefault("output-file", inputPath.toString().replace(".enc", ".dec")));
+        String outputFile = arguments.getOrDefault("output-file", inputPath.toString().replace(".enc", ""));
+        Path outputPath = Paths.get(((outputFile.endsWith(".dec"))?outputFile:(outputFile+".dec")));
 
         try (InputStream encryptedData = Files.newInputStream(inputPath);
              CipherInputStream decryptStream = new CipherInputStream(encryptedData, cipher);
@@ -83,6 +120,7 @@ public class Part1 {
             }
         } catch (IOException ex) {
             LOG.log(Level.SEVERE, "Unable to decrypt", ex);
+            exit(1);
         }
 
         LOG.info("Decryption complete, saved at " + outputPath);
@@ -97,11 +135,24 @@ public class Part1 {
      * @throws InvalidAlgorithmParameterException
      * @throws InvalidKeyException
      */
-    private static void performEncryption(Cipher cipher, IvParameterSpec iv, SecretKeySpec skeySpec, Map<String, String> arguments) throws InvalidAlgorithmParameterException, InvalidKeyException {
-        cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+    private static void performEncryption(Cipher cipher, IvParameterSpec iv, SecretKeySpec skeySpec, Map<String, String> arguments, String cipherMode){
+        try {
+            if (cipherMode.contains("GCM")) {
+                cipher.init(Cipher.ENCRYPT_MODE, skeySpec, new GCMParameterSpec(GCM_TAG_LENGTH, iv.getIV()));
+            } else {
+                cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+            }
+        }catch (InvalidKeyException e) {
+            LOG.log(Level.SEVERE, "Invalid key. Cannot be used for encryption of this file.");
+            exit(1);
+        } catch (InvalidAlgorithmParameterException e) {
+            LOG.log(Level.SEVERE, "Invalid algorithm. Cannot be used for encryption of this file.");
+            exit(1);
+        }
 
         Path inputPath = Paths.get(arguments.getOrDefault("input-file", null));
-        Path outputPath = Paths.get(arguments.getOrDefault("output-file", inputPath.toString() + ".enc"));
+        String outputFile = arguments.getOrDefault("output-file", inputPath.toString());
+        Path outputPath = Paths.get(((outputFile.endsWith(".enc"))?outputFile:(outputFile+".enc")));
 
         try (InputStream fin = Files.newInputStream(inputPath);
              OutputStream fout = Files.newOutputStream(outputPath);
@@ -113,9 +164,10 @@ public class Part1 {
             }
         } catch (IOException e) {
             LOG.log(Level.SEVERE, "Unable to encrypt", e);
+            exit(1);
         }
 
-        LOG.info("Encryption finished, saved at " + outputPath);
+        LOG.info("Encryption completed, saved at " + outputPath);
     }
 
     /**
@@ -124,21 +176,29 @@ public class Part1 {
      * @param sr - the secure random instance
      * @return the secret key spec is returned.
      */
-    private static SecretKeySpec getOrCreateKey(Map<String, String> arguments, SecureRandom sr) {
-        byte[] key = new byte[16];
-        if(arguments.containsKey("key")) {
-            key = arguments.get("key").getBytes();
+    private static SecretKeySpec getOrCreateKey(Map<String, String> arguments, SecureRandom sr){
+        byte[] key = new byte[16]; // Default key size
+        if (arguments.containsKey("key")) {
+            Path keyPath = Paths.get(arguments.get("key"));
+            try {
+                key = Base64.getDecoder().decode(Files.readAllBytes(keyPath));
+            } catch (IOException e) {
+                LOG.log(Level.SEVERE, "Unable to read key or file "+arguments.get("key")+" does not exist.");
+                exit(1);
+            }
+            if (key.length != 16 && key.length != 24 && key.length != 32) {
+                LOG.log(Level.SEVERE, "Invalid AES key length: " + key.length + " bytes. Must be 16, 24, or 32 bytes.");
+                exit(1);
+            }
             System.out.println("Given key=" + Util.bytesToHex(key));
-        }
-        else{
+        } else {
             sr.nextBytes(key); // 128 bit key
             System.out.println("Random key=" + Util.bytesToHex(key));
+            saveBase64File(key, "key"+((keyIncrement==0)?"":keyIncrement)+".base64");
+            ++keyIncrement;
         }
 
-        saveBase64File(key, "key.base64"); // Saves the key byte data
-
-        SecretKeySpec skeySpec = new SecretKeySpec(key, ALGORITHM);
-        return skeySpec;
+        return new SecretKeySpec(key, ALGORITHM);
     }
 
     /**
@@ -149,19 +209,28 @@ public class Part1 {
      */
     private static IvParameterSpec getOrCreateIv(Map<String, String> arguments, SecureRandom sr) {
         byte[] initVector = new byte[16];
-        if(arguments.containsKey("initialisation-vector")) {
-            initVector = arguments.get("initialisation-vector").getBytes();
+        if (arguments.containsKey("initialisation-vector")) {
+            Path ivPath = Paths.get(arguments.get("initialisation-vector"));
+            try {
+                initVector = Base64.getDecoder().decode(Files.readAllBytes(ivPath));
+            } catch (IOException e) {
+                LOG.log(Level.SEVERE, "Unable to read initialisation-vector or file \'"+ arguments.get("initialisation-vector") +"\' does not exist");
+                exit(1);
+            }
+            if (initVector.length != 16) {
+               LOG.log(Level.SEVERE, "Invalid IV length: " + initVector.length + " bytes. Must be 16 bytes.");
+                exit(1);
+            }
             System.out.println("Given initVector=" + Util.bytesToHex(initVector));
-        }
-        else{
+            LOG.log(Level.INFO, "IV has been successfully generated.");
+        } else {
             sr.nextBytes(initVector); // 16 bytes IV
             System.out.println("Random initVector=" + Util.bytesToHex(initVector));
+            saveBase64File(initVector, "iv"+((ivIncrement==0)?"":ivIncrement)+".base64");
+            ++ivIncrement;
         }
 
-        saveBase64File(initVector, "iv.base64"); // Saves the iv byte data
-
-        IvParameterSpec iv = new IvParameterSpec(initVector);
-        return iv;
+        return new IvParameterSpec(initVector);
     }
 
     /**
@@ -171,7 +240,11 @@ public class Part1 {
      */
     private static void saveBase64File(byte[] data, String filename) {
         try {Files.write(Paths.get(filename), Base64.getEncoder().encode(data));}
-        catch (IOException e) {throw new RuntimeException(e);}
+        catch (IOException e) {
+            LOG.log(Level.SEVERE, "Unable to save data to "+filename);
+            exit(1);
+        }
+        LOG.log(Level.INFO, "Successfully created base64 file. Saved to: " + filename);
     }
 
     /**
@@ -195,6 +268,7 @@ public class Part1 {
                 params.put(key, value);
             } else {
                 LOG.severe("Argument " + args[i] + " is missing a value.");
+                exit(1);
             }
         }
         return params;
