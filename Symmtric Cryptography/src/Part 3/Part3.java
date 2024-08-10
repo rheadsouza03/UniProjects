46a18f15825dfe42ad6c4b4d099a91a41c1df5b9
Rhea D'Souza <97720582+rheadsouza03@users.noreply.github.com>
2024-08-11 00:27:50 +1200

    The encryption and decryption process has been sorted. Now the writing to file needs to be sorted.

--- Symmtric Cryptography/src/Part 3/Part3.java
+++ Symmtric Cryptography/src/Part 3/Part3.java
@@ -27,39 +27,77 @@ public class Part3 {
     private static final Logger LOG = Logger.getLogger(Part3.class.getSimpleName());
 
     private static final String ALGORITHM = "AES";
-    private static final List<String> CIPHERS = List.of(
+    private static final String[] CIPHERS = {
             "AES/CBC/PKCS5Padding",
             "AES/ECB/PKCS5Padding",
             "AES/GCM/NoPadding",
             "AES/OFB/NoPadding",
             "AES/CFB/NoPadding"
-    );
+    };
     private static final int GCM_TAG_LENGTH = 128;
     private static final int[] KEY_SIZES = {128, 192, 256}; // in bits
+    private static final String[] INPUT_FILES = {"plaintext.txt", "plaintext2.txt", "plaintext3.txt", "plaintext4.txt", "plaintext5.txt"};
 
     public static void main(String[] args){
-        SecureRandom sr = new SecureRandom();
-        byte[] key = new byte[(int)(KEY_SIZES[0]/8)]; // Default key size
-        sr.nextBytes(key);
-        SecretKeySpec skeySpec = new SecretKeySpec(key, ALGORITHM);
+        Map<int[], Double> encryptionDurations = new HashMap<>();
+        Map<int[], Double> decryptionDurations = new HashMap<>();
 
-        byte[] initVector = new byte[16];
-        sr.nextBytes(initVector);
-        IvParameterSpec iv = new IvParameterSpec(initVector);
+        for(int i = 0; i < INPUT_FILES.length; i++){
+            for(int j = 0; j < KEY_SIZES.length; j++){
+                for(int k = 0; k < CIPHERS.length; k++){
+                    SecureRandom sr = new SecureRandom();
 
-        System.out.println("Random key=" + Util.bytesToHex(key));
-        System.out.println("initVector=" + Util.bytesToHex(initVector));
+                    // Create random key
+                    byte[] key = new byte[(int)( KEY_SIZES[j] / 8)]; // Default key size
+                    sr.nextBytes(key);
+                    SecretKeySpec skeySpec = new SecretKeySpec(key, ALGORITHM);
 
-        // Gets mode of Cipher
-        String cipherMode = CIPHERS.get(0);
+                    // Create random initialisation-vector
+                    byte[] initVector = new byte[16];
+                    sr.nextBytes(initVector);
+                    IvParameterSpec iv = new IvParameterSpec(initVector);
 
-        // Get pre-initialised Cipher instance
-        Cipher cipherEnc = getCipher(cipherMode, Cipher.ENCRYPT_MODE, skeySpec, iv);
-        Cipher cipherDec = getCipher(cipherMode, Cipher.DECRYPT_MODE, skeySpec, iv);
+                    // Print random values to console/command-line
+                    System.out.println("Random key=" + Util.bytesToHex(key));
+                    System.out.println("initVector=" + Util.bytesToHex(initVector));
 
-        //Perform Encryption or Decryption
-        performEncryption(cipherEnc, iv, skeySpec, "plaintext.txt", cipherMode);
-        performDecryption(cipherDec, iv, skeySpec, "plaintext.txt", cipherMode);
+                    // Get pre-initialised Cipher instance
+                    String cipher = CIPHERS[k];
+                    Cipher cipherEnc = getCipher(cipher, Cipher.ENCRYPT_MODE, skeySpec, iv);
+                    Cipher cipherDec = getCipher(cipher, Cipher.DECRYPT_MODE, skeySpec, iv);
+
+                    // Perform multiple iterations of encryption and decryption and collect their durations
+                    double[] iterDurations = getAverageDurations(cipherEnc, cipherDec, INPUT_FILES[i]);
+
+                    // Store collected durations
+                    encryptionDurations.put(new int[]{i, j, k}, iterDurations[0]);
+                    decryptionDurations.put(new int[]{i, j, k}, iterDurations[1]);
+                }
+            }
+        }
+
+    }
+
+    /**
+     * Gets the average duration taken to perform encryption and decryption with the given values.
+     * @param cipherEnc - Cipher instance initialised for encryption
+     * @param cipherDec - Cipher instance initialised for encryption
+     * @param inputFile - String with the input file name
+     * @return Double array with encryption average stored at index 0 and decryption stored in index 1
+     */
+    private static double[] getAverageDurations(Cipher cipherEnc, Cipher cipherDec, String inputFile) {
+        double[] durations = {0, 0};
+
+        for(int i = 0; i < 10; i++){
+            //Perform Encryption and Decryption, and get the summation of their durations
+            durations[0] += performOperation(cipherEnc, inputFile, "encryption");
+            durations[1] += performOperation(cipherDec, inputFile, "decryption");
+        }
+
+        durations[0] /= 10;
+        durations[1] /= 10;
+
+        return durations;
     }
 
     /**
@@ -104,119 +142,46 @@ public class Part3 {
      * Saves to given file with ending suffix `.dec`, if output file not provided,
      * the input file is used for it's naming.
      * @param cipher - Cipher instance used to perform decryption
-     * @param iv - Initialisation-vector that was mandatory to provide
-     * @param skeySpec - Key that was mandatory to provide
      * @param inputFile - file containing the encrypted/ciphertext data
      */
-    private static void performDecryption(Cipher cipher, IvParameterSpec iv, SecretKeySpec skeySpec, String inputFile, String cipherMode){
+    private static double performOperation(Cipher cipher, String inputFile, String operation){
         try {
-            if (cipherMode.contains("GCM")) {
-                cipher.init(Cipher.DECRYPT_MODE, skeySpec, new GCMParameterSpec(GCM_TAG_LENGTH, iv.getIV()));
-            }
-            else if (cipherMode.contains("ECB")){cipher.init(Cipher.DECRYPT_MODE, skeySpec);}
-            else {cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);}
-
             // Gets the file path
             Path inputPath = Paths.get("data/"+inputFile);
 
-            // Decrypting: ciphertext -> plaintext
-            byte[] ciphertext = cipher.doFinal(Files.readAllBytes(inputPath));
+            // Performs encryption/decryption and measures duration
+            double start = System.currentTimeMillis();
+            cipher.doFinal(Files.readAllBytes(inputPath));
+            double end = System.currentTimeMillis();
 
-        }catch (InvalidKeyException e) {
-            LOG.log(Level.SEVERE, "Invalid key. Cannot be used for decryption of this file.");
-            exit(1);
-        } catch (InvalidAlgorithmParameterException e) {
-            LOG.log(Level.SEVERE, "Invalid algorithm. Cannot be used for decryption of this file.");
-            exit(1);
-        }catch (IllegalBlockSizeException e) {
-            LOG.severe("Unable to decrypt: Illegal block size. Cannot perform encryption.");
+            return end - start;
+        } catch (IllegalBlockSizeException e) {
+            LOG.severe("Unable to "+operation+": Illegal block size. Cannot perform encryption.");
             exit(1);
         } catch (BadPaddingException e) {
-            LOG.severe("Unable to decrypt: Bad padding. Cannot perform encryption.");
+            LOG.severe("Unable to "+operation+": Bad padding. Cannot perform encryption.");
             exit(1);
         }catch (IOException e) {
-            LOG.severe("Unable to decrypt: Error occurred when reading or writing to a file.");
+            LOG.severe("Unable to perform "+operation+": Error occurred when reading or writing to a file.");
             exit(1);
         }
+
+        return 0;
     }
 
-    /**
-     * Encrypts the input-file and saves the ciphertext to the given output file.
-     * Otherwise, creates an output file using the input file as its prefix.
-     * @param cipher - Cipher instance for encryption
-     * @param iv - Initialisation vector
-     * @param skeySpec - Secret key spec
-     * @param inputFile - the file that containing the plaintext data
-     */
-    private static void performEncryption(Cipher cipher, IvParameterSpec iv, SecretKeySpec skeySpec, String inputFile, String cipherMode){
-        try {
-            // Appropriately initialise cipher instance
-            if (cipherMode.contains("GCM")) {
-                cipher.init(Cipher.ENCRYPT_MODE, skeySpec, new GCMParameterSpec(GCM_TAG_LENGTH, iv.getIV()));
-            }
-            else if (cipherMode.contains("ECB")){cipher.init(Cipher.ENCRYPT_MODE, skeySpec);}
-            else {cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);}
-
-            // Gets the file path
-            Path inputPath = Paths.get("data/"+inputFile);
-
-            // Encrypting: plaintext -> ciphertext
-            byte[] ciphertext = cipher.doFinal(Files.readAllBytes(inputPath));
-
-        }catch (InvalidKeyException e) {
-            LOG.log(Level.SEVERE, "Invalid key. Cannot be used for encryption of this file.");
-            exit(1);
-        } catch (InvalidAlgorithmParameterException e) {
-            LOG.log(Level.SEVERE, "Invalid algorithm. Cannot be used for encryption of this file.");
-            exit(1);
-        }catch (IllegalBlockSizeException e) {
-            LOG.severe("Unable to encrypt: Illegal block size. Cannot perform encryption.");
-            exit(1);
-        } catch (BadPaddingException e) {
-            LOG.severe("Unable to encrypt: Bad padding. Cannot perform encryption.");
-            exit(1);
-        }catch (IOException e) {
-            LOG.severe("Unable to encrypt: Error occurred when reading or writing to a file.");
-            exit(1);
+    private static int[] getFileSizes(){
+        int[] fileSizes = new int[INPUT_FILES.length];
+        for(int i = 0; i < INPUT_FILES.length; i++){
+            fileSizes[i] = (int) new File("data/"+INPUT_FILES[i]).length();
         }
+
+        return fileSizes;
     }
 
-    /**
-     * Looks through the .base64 files in the directory with the matching prefix. Aims to find a pattern match and
-     * use the number in the file to determine the next increment for a new .base64 file.
-     * @param prefix - file prefix
-     * @return Integer related to the number of files with the given prefix in the current directory
-     */
-    private static int getMaxIncrement(String prefix) {
-        File directory = new File("./data/");
-        File[] files = directory.listFiles((_, name) -> name.startsWith(prefix) && name.endsWith(".base64"));
-
-        if (files == null || files.length == 0) {
-            return 0;
-        }
-
-        Pattern pattern = Pattern.compile("^"+prefix + "(\\d+)" + "\\.base64$");
-        int maxIncrement = 0;
-
-        for (File file : files) {
-            Matcher matcher = pattern.matcher(file.getName());
-            if (matcher.matches()) {
-                String match = matcher.group(1);
-                int increment = Integer.parseInt(match);
-                if (increment > maxIncrement) {
-                    maxIncrement = increment;
-                }
-            }
-        }
-
-        return maxIncrement+1;
-    }
-
-    private static void savePerformanceMetrics(String operation, byte[] key, String mode, long duration) {
+    private static void savePerformanceMetrics(String operation, byte[] key, String mode, int fileSize, double duration) {
         String csvFile = "results.csv";
-        String keyLength = String.valueOf(Base64.getDecoder().decode(key));
-        String fileSize = String.valueOf(new File("data/plaintext.txt").length());
-        String entry = String.format("%s,%s,%s,%s,%d\n", operation, mode, fileSize, keyLength, duration);
+        String keyLength = Arrays.toString(Base64.getDecoder().decode(key));
+        String entry = String.format("%s,%s,%d,%s,%.2f\n", operation, mode, fileSize, keyLength, duration);
 
         try {
             Files.write(Paths.get(csvFile), entry.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
